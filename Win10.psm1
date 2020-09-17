fcff0ef76efa70e4389b141d0060b855cd8266ee
StanleyBublick <hodlm8@protonmail.com>
2020-09-17 19:10:41 +0300

    Added Chocolatey installer tweak

--- Win10.psm1
+++ Win10.psm1
@@ -4078,6 +4078,140 @@ Function UnpinTaskbarIcons {
 
 
 ##########
+#region Packages
+##########
+
+# Install chocolatey and packages from Packages.config
+Function InstallChocoWithPackages {
+	$scriptPath = $PSScriptRoot
+
+	if (!(Get-Command choco -errorAction SilentlyContinue)) {
+		Write-Output "Installing Chocolatey..."
+		Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	}
+	
+	Write-Output "Installing Packages from $scriptPath\Packages.config..."
+	choco install "$scriptPath\Packages.config" --confirm --acceptlicense
+}
+
+# Uninstall chocolatey with all packages
+# WARNING!! This will remove Chocolatey and all packages, software, 
+# and configurations in the Chocolatey Installation folder from your machine. 
+# Everything will be GONE. This is very destructive. DO NOT RUN this tweak unless 
+# you completely understand what the intention of this tweak is and are good with it.
+# If you mess something up, we cannot help you fix it.
+# Seriously, this tweak may destroy your machine and require a rebuild. 
+# It may have varied results on different machines in the same environment. 
+# Think twice before running this.
+Function UninstallChocoWithPackages {
+	$VerbosePreference = 'Continue'
+	if (-not $env:ChocolateyInstall) {
+		$message = @(
+			"The ChocolateyInstall environment variable was not found."
+			"Chocolatey is not detected as installed. Nothing to do."
+		) -join "`n"
+
+		Write-Warning $message
+		return
+	}
+
+	if (-not (Test-Path $env:ChocolateyInstall)) {
+		$message = @(
+			"No Chocolatey installation detected at '$env:ChocolateyInstall'."
+			"Nothing to do."
+		) -join "`n"
+
+		Write-Warning $message
+		return
+	}
+
+	<#
+		Using the .NET registry calls is necessary here in order to preserve environment variables embedded in PATH values;
+		Powershell's registry provider doesn't provide a method of preserving variable references, and we don't want to
+		accidentally overwrite them with absolute path values. Where the registry allows us to see "%SystemRoot%" in a PATH
+		entry, PowerShell's registry provider only sees "C:\Windows", for example.
+	#>
+	$userKey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Environment')
+	$userPath = $userKey.GetValue('PATH', [string]::Empty, 'DoNotExpandEnvironmentNames').ToString()
+
+	$machineKey = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SYSTEM\ControlSet001\Control\Session Manager\Environment\')
+	$machinePath = $machineKey.GetValue('PATH', [string]::Empty, 'DoNotExpandEnvironmentNames').ToString()
+
+	$backupPATHs = @(
+		"User PATH: $userPath"
+		"Machine PATH: $machinePath"
+	)
+	$backupFile = "C:\PATH_backups_ChocolateyUninstall.txt"
+	$backupPATHs | Set-Content -Path $backupFile -Encoding UTF8 -Force
+
+	$warningMessage = @(
+		"This could cause issues after reboot where nothing is found if something goes wrong."
+		"In that case, look at the backup file for the original PATH values in '$backupFile'."
+	) -join "`n"
+
+	if ($userPath -like "*$env:ChocolateyInstall*") {
+		Write-Verbose "Chocolatey Install location found in User Path. Removing..."
+		Write-Warning $warningMessage
+
+		$newUserPATH = @(
+			$userPath -split [System.IO.Path]::PathSeparator |
+				Where-Object { $_ -and $_ -ne "$env:ChocolateyInstall\bin" }
+		) -join [System.IO.Path]::PathSeparator
+
+		# NEVER use [Environment]::SetEnvironmentVariable() for PATH values; see https://github.com/dotnet/corefx/issues/36449
+		# This issue exists in ALL released versions of .NET and .NET Core as of 12/19/2019
+		$userKey.SetValue('PATH', $newUserPATH, 'ExpandString')
+	}
+
+	if ($machinePath -like "*$env:ChocolateyInstall*") {
+		Write-Verbose "Chocolatey Install location found in Machine Path. Removing..."
+		Write-Warning $warningMessage
+
+		$newMachinePATH = @(
+			$machinePath -split [System.IO.Path]::PathSeparator |
+				Where-Object { $_ -and $_ -ne "$env:ChocolateyInstall\bin" }
+		) -join [System.IO.Path]::PathSeparator
+
+		# NEVER use [Environment]::SetEnvironmentVariable() for PATH values; see https://github.com/dotnet/corefx/issues/36449
+		# This issue exists in ALL released versions of .NET and .NET Core as of 12/19/2019
+		$machineKey.SetValue('PATH', $newMachinePATH, 'ExpandString')
+	}
+
+	# Adapt for any services running in subfolders of ChocolateyInstall
+	$agentService = Get-Service -Name chocolatey-agent -ErrorAction SilentlyContinue
+	if ($agentService -and $agentService.Status -eq 'Running') {
+		$agentService.Stop()
+	}
+	# TODO: add other services here
+
+	Remove-Item -Path $env:ChocolateyInstall -Recurse -Force
+
+	'ChocolateyInstall', 'ChocolateyLastPathUpdate' | ForEach-Object {
+		foreach ($scope in 'User', 'Machine') {
+			[Environment]::SetEnvironmentVariable($_, [string]::Empty, $scope)
+		}
+	}
+
+	if ($env:ChocolateyToolsLocation -and (Test-Path $env:ChocolateyToolsLocation)) {
+		Remove-Item -Path $env:ChocolateyToolsLocation -Recurse -Force
+	}
+	
+	foreach ($scope in 'User', 'Machine') {
+		[Environment]::SetEnvironmentVariable('ChocolateyToolsLocation', [string]::Empty, $scope)
+	}
+
+	$machineKey.Close()
+	$userKey.Close()
+	
+}
+
+##########
+#endregion Packages
+##########
+
+
+
+##########
 #region Auxiliary Functions
 ##########
 
